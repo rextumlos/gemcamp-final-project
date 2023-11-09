42b37cdbc553d7b83c9d3cb5e5710ee3eb1aca7d
rextumlos <johnrex412@gmail.com>
2023-11-09 17:37:24 +0800

    [Feature 8] Counter Cache: Used to count children members

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,6 +16,8 @@ class User < ApplicationRecord
 
   mount_uploader :image, ImageUploader
 
+  belongs_to :parent, class_name: 'User', optional: true, counter_cache: :children_members
+  has_many :children, class_name: 'User', foreign_key: 'parent_id'
   has_many :addresses
 
 end
