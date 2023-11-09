4df382cce518e84c09c6bfec458fd2dec7809192
rextumlos <johnrex412@gmail.com>
2023-11-09 15:26:04 +0800

    Fix: Bootstrap in application.js for bootstrap togglers

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,7 +1,15 @@
 // Entry point for the build script in your package.json
 import "@hotwired/turbo-rails"
+import * as bootstrap from "bootstrap"
 import "./controllers"
 
 import jQuery from "jquery"
 window.jQuery = jQuery
-window.$ = jQuery 
+window.$ = jQuery
+
+document.addEventListener("turbo:load", () => {
+  var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+  var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+    return new bootstrap.Tooltip(tooltipTriggerEl)
+  })
+})
